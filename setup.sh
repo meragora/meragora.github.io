e68c523c932b8d88f456a9659f1bcdf866338d90
meragora <52658854+meragora@users.noreply.github.com>
2024-07-16 15:28:34 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Update package list
+sudo apt-get update
+# Install build-essential and other necessary packages
+sudo apt-get install -y build-essential libssl-dev libffi-dev python3-dev
