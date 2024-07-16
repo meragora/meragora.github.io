90c2d997146600609c91b87c82219af9a9a7cf34
meragora <52658854+meragora@users.noreply.github.com>
2024-07-16 15:31:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Update package list
-sudo apt-get update
+apt-get update
 # Install build-essential and other necessary packages
-sudo apt-get install -y build-essential libssl-dev libffi-dev python3-dev
+apt-get install -y build-essential libssl-dev libffi-dev python3-dev
